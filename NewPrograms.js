f08130f57e9f04262558dd6e387b113da613c8f3
Manasi Sant <manasisant.27@gmail.com>
2022-01-16 23:39:52 +0530

    use of String literals, if else control structures

---
+++ NewPrograms.js
@@ -0,0 +1,86 @@
+// // // // // const fixAge = 16;
+// // // // // console.log(fixAge);
+
+// // // // // let ageOfManu = fixAge - 7;
+// // // // // let ageOfMrunu = fixAge - 3;
+// // // // // console.log(ageOfManu, ageOfMrunu);
+
+// // // // // if (ageOfManu <= ageOfMrunu) alert("Manali is elder sister of Mrunu");
+// // // // // console.log("Manali is younger sister of Mrunu");
+
+// // // // //For Test DATA 1
+// // // // let x = (weightOfMark = 78);
+// // // // let y = (weightOfJohn = 92);
+// // // // console.log("Weight of Mark is" + " " + x + "Kg");
+// // // // console.log("Weight of John is" + " " + y + "Kg");
+
+// // // // let heightOfMark = 1.69;
+// // // // let heightOfJohn = 1.95;
+// // // // console.log("Height of Mark is" + " " + heightOfMark + "m");
+// // // // console.log("Height of John is" + " " + heightOfJohn + "m");
+
+// // // // let bmiOfMark = x / heightOfMark ** 2; //mass / (height * height)
+// // // // let bmiOfJohn = x / (heightOfJohn * heightOfJohn);
+// // // // console.log("Mark's BMI" + " " + bmiOfMark);
+// // // // console.log("John's BMI" + " " + bmiOfJohn);
+// // // // const markHigherBMI = bmiOfMark > bmiOfJohn;
+// // // // console.log(markHigherBMI);
+
+// // // // //case 2
+// // // // // let x = 95;
+// // // // // let y = 85;
+// // // // // console.log(x, y);
+// // // const firstName = "Manasi";
+// // // const age = 1997;
+// // // const year = 2021;
+
+// // // const manasi = "I'm " + firstName + ", a  " + (year - age) + " year old girl.";
+// // // console.log(manasi);
+
+// // // const manasiNew = `I'm ${firstName}, a ${year - age} old girl.`;
+// // // console.log(manasiNew);
+// // // console.log("Just a normal string..");
+
+// // // console.log("String \n with multiple \n lines");
+
+// // // console.log(`String
+// // // always
+// // // works
+// // // the
+// // // bestttt..`);
+
+// // const age = 14;
+
+// // if (age >= 18) {
+// //   console.log(`Sarah is eligible to use driving license🚗`);
+// // } else {
+// //   const yearsLeft = 18 - age;
+// //   console.log(`Sarah is too young. Wait another ${yearsLeft} Years`);
+// // }
+
+// // //if else control structure
+// // // if (){
+
+// // // } else {
+
+// // // }
+
+// const birthYear = 1997;
+// let century;
+// if (birthYear >= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(century);
+
+const markBMI = 28.3;
+console.log(`Mark's BMI is ${markBMI}`);
+const johnBMI = 23.9;
+console.log(`John's BMI is ${johnBMI}`);
+
+if (markBMI > johnBMI) {
+  console.log(`Mark's BMI is higher than John's BMI!`);
+} else {
+  console.log(`Mark's BMI is less than John's BMI!`);
+}
---
+++ script.js
@@ -0,0 +1,61 @@
+// /*let js = "amazing";
+// if (js == "amazing") alert("JavaScript is fun!");
+// // console.log(40 + 5);
+// // console.log("Printing string...");
+
+// let firstName = "Manali";
+// console.log(firstName);
+
+// let lastName = "Sant";
+// console.log(lastName);
+// console.log(firstName);
+// console.log(lastName);
+// */
+
+// /*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+// console.log(typeof true);
+// // console.log(typeof "Manasi");
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 50);
+
+// javascriptIsFun = "MAN";
+// console.log(typeof javascriptIsFun);
+// */
+
+// // let age = 30; //use of let is done to assign value
+// // age = 31;
+// // console.log(age);
+
+// // const panCardNumber = 31222535559; //use of const is done to assign value
+// // console.log(panCardNumber);
+
+const currentYear = 2022;
+const ageManasi = currentYear - 1997;
+const ageJonas = currentYear - 1973;
+console.log(ageManasi, ageJonas);
+
+// const currentAge = 2022;
+
+// const ageManali = currentAge - 1997;
+// const ageMrunmesh = currentAge - 2006;
+// console.log(ageManali, ageMrunmesh);
+
+// // const firstName = "Thinner";
+// // const lastName = "Kyle";
+
+// // console.log(firstName + " " + lastName);
+
+// //Assignment Operators
+// let x = 30 + 6;
+// x += 100;
+// x++;
+// console.log(x);
+
+// //comparison operators
+
+// console.log(ageManasi >= 18);
+
+// const isFullAge = ageManasi >= 18;
+// console.log(isFullAge);
