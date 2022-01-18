89d09ab3467ad89edad1f21740a5d26ea7bcbf1a
Manasi Mahesh Sant <manasisant@outlook.com>
2022-01-18 22:56:50 +0530

    if else structure added with example

---
+++ exampletask.js
@@ -0,0 +1,33 @@
+// const enterTheMonth = Number(prompt("Enter the month"));
+// console.log(typeof enterTheMonth);
+// console.log(enterTheMonth);
+
+let anyMonth = prompt("Enter the month!");
+
+if (anyMonth == 1) {
+  console.log("January");
+} else if (anyMonth == 2) {
+  console.log("February");
+} else if (anyMonth == 3) {
+  console.log("March!!!");
+} else if (anyMonth == 4) {
+  console.log("Aprillllll!");
+} else if (anyMonth == 5) {
+  console.log("Mayyy!!");
+} else if (anyMonth == 6) {
+  console.log("June!");
+} else if (anyMonth == 7) {
+  console.log("July");
+} else if (anyMonth == 8) {
+  console.log("August");
+} else if (anyMonth == 9) {
+  console.log("September");
+} else if (anyMonth == 10) {
+  console.log("October");
+} else if (anyMonth == 11) {
+  console.log("November");
+} else if (anyMonth == 12) {
+  console.log("Dec");
+} else {
+  console.log("Invalid Input");
+}
---
+++ switchcase.js
@@ -0,0 +1,42 @@
+let enterMonth = prompt("Enter the month");
+
+switch (enterMonth) {
+  case "1":
+    console.log("January");
+    break;
+  case "2":
+    console.log("Febuary");
+    break;
+  case "3":
+    console.log("March");
+    break;
+  case "4":
+    console.log("Aprilll!");
+    break;
+  case "5":
+    console.log("May!");
+    break;
+  case "6":
+    console.log("June");
+    break;
+  case "7":
+    console.log("July!");
+    break;
+  case "8":
+    console.log("August");
+    break;
+  case "9":
+    console.log("Septemberr");
+    break;
+  case "10":
+    console.log("October!!!");
+    break;
+  case "11":
+    console.log("November");
+    break;
+  case "12":
+    console.log("Decemberrrrr!");
+    break;
+  default:
+    console.log("Invalid Input!");
+}
